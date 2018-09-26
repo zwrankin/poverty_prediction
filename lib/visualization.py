8458dae2030653612cb8305519f5028b2f49710a
Zane Rankin <zrankin@ihme.washington.edu>
2018-09-26 08:42:01 -0500

    add report_cv_scores function

--- lib/visualization.py
+++ lib/visualization.py
@@ -74,6 +74,18 @@ def cv_model(transformer_pipeline, alg, X_train, y_train, cv, scoring, model_res
         return model_results
 
 
+def report_cv_scores(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
+
+
 def compare_algorithm_cv_scores(transformer_pipeline, algs, X_train, y_train, cv, scoring):
     # Filter out warnings from models
     warnings.filterwarnings('ignore', category=ConvergenceWarning)
