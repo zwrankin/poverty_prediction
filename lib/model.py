ee64a8766ad916c7e73b704dcf86f0bb697d87d1
Zane Rankin <zrankin@ihme.washington.edu>
2018-10-03 11:30:13 -0500

    update load_and_process functions

--- lib/model.py
+++ lib/model.py
@@ -8,6 +8,7 @@ from sklearn.metrics import f1_score, make_scorer
 from sklearn.model_selection import StratifiedKFold
 
 from .preprocessing import preprocess
+from .preprocessing import run_feature_engineering
 from .preprocessing import feature_engineer_rent, feature_engineer_education, feature_engineer_demographics
 from .preprocessing import feature_engineer_house_rankings, feature_engineer_house_characteristics, feature_engineer_assets
 from .pipeline import FeatureEngineer
@@ -32,7 +33,7 @@ transformer_pipeline = Pipeline(steps=[
                         ('feature_scaler', StandardScaler()),
                         ])
 
-full_pipeline = Pipeline(steps=[
+pipeline_rf = Pipeline(steps=[
                         ('features', feature_extraction),
                         ('imputer', Imputer(strategy='mean')),
                         ('feature_scaler', StandardScaler()),
@@ -40,30 +41,50 @@ full_pipeline = Pipeline(steps=[
 
 LEVELS = ['low', 'medium']
 DEPTHS = [20, 30, 40]
-param_grid = dict(features__extract_educ__level=LEVELS,
+param_grid_rf = dict(features__extract_educ__level=LEVELS,
                  features__extract_rent__level=LEVELS,
                  features__extract_demog__level=LEVELS,
                  features__extract_houseq__level=LEVELS,
                  features__extract_housec__level=LEVELS,
                  features__extract_assets__level=LEVELS,
                  random_forest__max_depth=DEPTHS,
-                 # svc__gamma=[0.01, 0.1,1,10],
-                 # svc__C=[0.001,0.01,0.1,1,10,100,1000]
                  )
 
 
-def load_and_process_training_data():
+def load_and_process_training_data(engineer_features=True, subset_to_hh=True):
     """
-    Loads training data
-    :return: two pd.DataFrames, [X_train, y_train]
+    Convenience function
+    :param engineer_features: whether to run feature engineering
+    :param subset_to_hh: whether to subset to heads of household
+    :return:  pd.DataFrames, [X_train, y_train]
     """
-    train = pd.read_csv('../input/train.csv')
 
-    # Note that subsetting X_train to heads of households happens during feature engineering, but subset y_train here
+    train = pd.read_csv('../input/train.csv')
+    hh_idx = train.parentesco1 == 1
+
     X_train = preprocess(train).drop('Target', axis=1)
-    y_train = train.query('parentesco1 == 1')['Target']
+    if engineer_features:
+        X_train = run_feature_engineering(X_train, level='medium')
+
+    y_train = train['Target']
+    if subset_to_hh:
+        X_train = X_train.loc[hh_idx]
+        y_train = y_train.loc[hh_idx]
 
     return X_train, y_train
+
+
+def load_and_process_test_data(engineer_features=True):
+    """
+    Convenience function
+    :param engineer_features: whether to run feature engineering
+    :return: pd.DataFrame X_test
+    """
+    test = pd.read_csv('../input/test.csv')
+    X_test = preprocess(test)
+    if engineer_features:
+        X_test = run_feature_engineering(X_test, level='medium')
+    return X_test
 
 
 if __name__ == 'main':
