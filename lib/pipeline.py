c8a79e43930035275bd93774a3660bff38b39c89
Zane Rankin <zrankin@ihme.washington.edu>
2018-10-07 23:00:09 -0500

    add hyperparameters to BaggedLGBMClassifier

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -44,65 +44,82 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
         return self.func(data, self.level, self.drop_correlated_features)
 
 
-def bag_and_boost_model(X, y, random_state, early_stopping_rounds=10):
-    """
-    Fits a gradient boosted model to bootstrapped data, and uses the unsampled (aka "out-of-bag") observations
-    as a validation set for early stopping
-    Different than sklearn's native BaggingClassifier because it allows early stopping
-    """
-
-    # First implementation is cleaner with pd.DataFrame
-    if isinstance(X, np.ndarray):
-        X = pd.DataFrame(X)
-    if isinstance(y, np.ndarray):
-        X = pd.DataFrame(y)
-
-    # Note that due to bootstrapping, sample_fraction=1 still leaves ~37% of data in the validation set
-    X_train, y_train = resample(X, y, n_samples=len(X), replace=True, random_state=random_state)
-    valid_idx = [i for i in X.index if i not in X_train.index]
-    X_valid = X.loc[valid_idx]
-    y_valid = y.loc[valid_idx]
-
-    tuned_params = {'boosting_type': 'dart',
-                    'colsample_bytree': 0.5796397953791418,
-                    'learning_rate': 0.08739537002929919,
-                    'min_child_samples': 15,
-                    'num_leaves': 48,
-                    'reg_alpha': 0.4239159481112283,
-                    'reg_lambda': 0.36419362906439723,
-                    'subsample_for_bin': 40000,
-                    'subsample': 0.986210861412967,
-                    'class_weight': 'balanced',
-                    'limit_max_depth': 1,
-                    'max_depth': 22}
-
-    fit_params = {"eval_set": [(X_valid, y_valid)],
-                  "early_stopping_rounds": early_stopping_rounds,
-                  "verbose": False}
-
-    clf = LGBMClassifier(**tuned_params, objective='multiclass',
-                         n_jobs=-1, n_estimators=10000,
-                         random_state=random_state)
-
-    return clf.fit(X_train, y_train, **fit_params)
-
-
 class BaggedLGBMClassifier(BaseEstimator, ClassifierMixin):
     """ADD DOCSTRING"""
 
-    def __init__(self, n_meta_estimators=5, early_stopping_rounds=10):
+    # TODO - these defaults are from old optimization, please update
+    def __init__(self, n_meta_estimators=5, early_stopping_rounds=10, random_state=0,
+                 boosting_type='dart', colsample_bytree=0.58, learning_rate=0.087, min_child_samples=15,
+                 num_leaves=48, reg_alpha=0.42, reg_lambda=0.36, subsample_for_bin=40000, subsample=0.99,
+                 class_weight='balanced', limit_max_depth=1, max_depth=22):
+
         self.n_meta_estimators = n_meta_estimators
         self.early_stopping_rounds = early_stopping_rounds
+        self.random_state = random_state
+        self.boosting_type = boosting_type
+        self.colsample_bytree = colsample_bytree
+        self.learning_rate = learning_rate
+        self.min_child_samples = min_child_samples
+        self.num_leaves = num_leaves
+        self.reg_alpha = reg_alpha
+        self.reg_lambda = reg_lambda
+        self.subsample_for_bin = subsample_for_bin
+        self.subsample = subsample
+        self.class_weight = class_weight
+        self.limit_max_depth = limit_max_depth
+        self.max_depth = max_depth
+
+    def bag_and_boost_model(self, X, y, random_state):
+        """
+        Fits a gradient boosted model to bootstrapped data, and uses the unsampled (aka "out-of-bag") observations
+        as a validation set for early stopping
+        Different than sklearn's native BaggingClassifier because it allows early stopping
+        """
+
+        # First implementation is cleaner with pd.DataFrame
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X)
+        if isinstance(y, np.ndarray):
+            X = pd.DataFrame(y)
+
+        # Note that due to bootstrapping, sample_fraction=1 still leaves ~37% of data in the validation set
+        X_train, y_train = resample(X, y, n_samples=len(X), replace=True, random_state=random_state)
+        valid_idx = [i for i in X.index if i not in X_train.index]
+        X_valid = X.loc[valid_idx]
+        y_valid = y.loc[valid_idx]
+
+        lgb_params = {'boosting_type': self.boosting_type,
+                      'colsample_bytree': self.colsample_bytree,
+                      'learning_rate': self.learning_rate,
+                      'min_child_samples': self.min_child_samples,
+                      'num_leaves': self.num_leaves,
+                      'reg_alpha': self.reg_alpha,
+                      'reg_lambda': self.reg_lambda,
+                      'subsample_for_bin': self.subsample_for_bin,
+                      'subsample': self.subsample,
+                      'class_weight': self.class_weight,
+                      'limit_max_depth': self.limit_max_depth,
+                      'max_depth': self.max_depth}
+
+        fit_params = {"eval_set": [(X_valid, y_valid)],
+                      "early_stopping_rounds": self.early_stopping_rounds,
+                      "verbose": False}
+
+        clf = LGBMClassifier(**lgb_params, objective='multiclass',
+                             n_jobs=-1, n_estimators=10000,
+                             random_state=random_state)
+
+        return clf.fit(X_train, y_train, **fit_params)
 
     def fit(self, X, y=None):
         if self.n_meta_estimators != 5:
             raise NotImplementedError('sorry, only 5 meta-estimators supported now')
 
-        clf1 = bag_and_boost_model(X, y, early_stopping_rounds=self.early_stopping_rounds, random_state=1)
-        clf2 = bag_and_boost_model(X, y, early_stopping_rounds=self.early_stopping_rounds, random_state=2)
-        clf3 = bag_and_boost_model(X, y, early_stopping_rounds=self.early_stopping_rounds, random_state=3)
-        clf4 = bag_and_boost_model(X, y, early_stopping_rounds=self.early_stopping_rounds, random_state=4)
-        clf5 = bag_and_boost_model(X, y, early_stopping_rounds=self.early_stopping_rounds, random_state=5)
+        clf1 = self.bag_and_boost_model(X, y, random_state=self.random_state + 1)
+        clf2 = self.bag_and_boost_model(X, y, random_state=self.random_state + 2)
+        clf3 = self.bag_and_boost_model(X, y, random_state=self.random_state + 3)
+        clf4 = self.bag_and_boost_model(X, y, random_state=self.random_state + 4)
+        clf5 = self.bag_and_boost_model(X, y, random_state=self.random_state + 5)
 
         self.clfs = [clf1, clf2, clf3, clf4, clf5]
 
